4affa96b247934fc8ee8f3682522f75b5bd6c1e6
sharhan-alhassan <sharhanalhassan@gmail.com>
2021-10-29 10:29:07 +0000

    implemented s3 interactions

--- src/backupdb/storage.py
+++ src/backupdb/storage.py
@@ -3,3 +3,13 @@ def local(infile, outfile):
     outfile.write(infile.read())
     outfile.close()
     infile.close()
+
+
+def s3(client, infile, bucket, name):
+    """
+    client: An AWS client object that has an upload_fileobj method.
+    infile: A file object with the data from our PostgreSQL backup.
+    bucket: The name of the bucket that we'll be storing the backup in.
+    name: The name of the file we'd like to create in our S3 bucket.
+    """
+    client.upload_fileobj(infile, bucket, name)
